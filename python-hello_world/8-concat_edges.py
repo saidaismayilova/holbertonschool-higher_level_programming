91b291bdfa3be4b1a0483adee87b7d0901c805d6
saidaismayilova <iseide40@gmail.com>
2025-11-16 11:45:14 +0000

    s

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[56:85]  # slice only the "object-oriented programming" part
-str += " with Python"  # add " with Python"
-print(str[56:64] + "-" + str[65:75] + str[75:85] + " with Python")
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str = str[39:66]
+print(str)
