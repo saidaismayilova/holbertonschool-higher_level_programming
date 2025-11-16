5e0e920091cbfc5103381718c155c880c5216de1
saidaismayilova <iseide40@gmail.com>
2025-11-16 18:23:16 +0000

    q

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     """Return a copy of the string without the character at index n"""
+    if n < 0 or n >= len(str):
+        return str  # indeks səhvdirsə orijinal stringi qaytar
     return str[:n] + str[n+1:]
-print(remove_char_at("Chicago", 3))  
+
+# Nümunə istifadə
+print(remove_char_at("Chicago", 3))  # Çap: Chiago
