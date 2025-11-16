47640509be95df90f6df11a2cadba68a561d10c4
saidaismayilova <iseide40@gmail.com>
2025-11-16 18:29:09 +0000

    q

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,20 +2,20 @@
 def remove_char_at(str, n):
     """
     Creates a copy of the string, removing the character at position n.
-    
+
     Args:
         str: The input string
         n: The position (0-indexed) of the character to remove
-    
+
     Returns:
         A new string with the character at position n removed
     """
     if n < 0 or n >= len(str):
         return str
-    
+
     new_str = ""
     for i in range(len(str)):
         if i != n:
             new_str += str[i]
-    
+
     return new_str
