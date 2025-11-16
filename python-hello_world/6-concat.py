797b33f3d7ecc5a0fbe3914bd22e67ebfacbb39a
saidaismayilova <iseide40@gmail.com>
2025-11-16 11:55:32 +0000

    s

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton School"
+str1 = "Holberton"
 str2 = "School"  # str2 artıq lazım deyil, amma test bunu istəyə bilər
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"Welcome to {str1}{str2}!")
