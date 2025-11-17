ff43090de4528efa005b7fd838e81a9ddd4eb58c
saidaismayilova <iseide40@gmail.com>
2025-11-17 13:20:19 +0000

    s

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, divif __name__ == "__main__":
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
     a = 10
     b = 5    print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
