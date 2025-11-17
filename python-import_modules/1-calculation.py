bf970b4a2a8d6fe65312405f5513d1e2d36fff6d
saidaismayilova <iseide40@gmail.com>
2025-11-17 13:01:08 +0000

    s

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if _name_ == _main_
+from 1-calculation import calculation
+a = 10
+b = 5
+return = add(a, b)
+print("{} + {} = {}".format(a, b, result))
+print("{} - {} = {}".format(a, b, result))
+print("{} / {} = {}".format(a, b, result))
+print("{} * {} = {}".format(a, b, result))
