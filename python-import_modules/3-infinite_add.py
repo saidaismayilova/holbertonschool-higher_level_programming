b394c331994bca3b6a9d307daa19eaf2d2895f99
saidaismayilova <iseide40@gmail.com>
2025-11-19 07:33:01 +0000

    salam

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    argv = sys.argv[1:]  # faylın özü sayı
-    total = 0
-    for num in argv:
-        total += int(num) 
-    print(total)
+argv = sys.argv[1:]  # faylın özü sayı
+total = 0
+for num in argv:
+total += int(num)
+print(total)
