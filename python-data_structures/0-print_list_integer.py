774f51d71e38c10841912831b5117fbb299cdbca
saidaismayilova <iseide40@gmail.com>
2025-11-20 09:54:05 +0000

    salam

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__"
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{}".format(num)))
