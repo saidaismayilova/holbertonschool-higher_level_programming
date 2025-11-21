2e559b8ba629390d04fcd13061a4f661b60e962e
saidaismayilova <iseide40@gmail.com>
2025-11-21 07:56:16 +0000

    salam

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
    if idx < 0 or if idx >= len(my_list):
-        return new_line = copy.my_list
+        return my_list.copy()
+    new_line = my_list.copy()
+    new_line[idx] = element
     return  new_line
