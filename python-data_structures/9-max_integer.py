f768518437aed168b36a1154b6814341f6c510bd
saidaismayilova <iseide40@gmail.com>
2025-11-23 18:29:23 +0000

    salam

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    biggest = my_list[0]
+
+    for num in my_list[1:]:
+        if num > biggest:
+            biggest = num
+
+    return biggest
