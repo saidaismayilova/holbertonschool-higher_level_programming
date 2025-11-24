263cd3102ff570e82711fd21bc1fe7d94b0e62d7
saidaismayilova <iseide40@gmail.com>
2025-11-24 13:59:42 +0000

    salam

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ = "__main__"
+"""Module that defines an empty Rectangle class."""
 class Rectangle:
-    """this is empty class"""
-pass
+    """Represents an empty Rectangle."""
+    pass
