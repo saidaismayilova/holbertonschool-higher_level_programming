4ef42aa5d1c9d6aea7f0df59da31236e55b83da1
saidaismayilova <iseide40@gmail.com>
2025-11-28 10:36:39 +0000

    salam

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,12 +5,11 @@ it is important
 
 
 class MyList(list):
-    ""This is class and this class have  a inherits"""
+   " ""This is class and this class have  a inherits"""
 
     def print_sorted(self):
     """"
         Siyahını artan sıralama qaydasında çap edir.
         Orijinal siyahıya eyisiklik etmir.
     """
-
-        print(sorted(self))
+print(sorted(self))
