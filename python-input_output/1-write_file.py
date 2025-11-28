5ed144286d6d51ef491b5226899fc04ff448fcd5
saidaismayilova <iseide40@gmail.com>
2025-11-28 19:51:07 +0000

    salam:

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Writes a string to a UTF-8 text file and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes text to a file and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
