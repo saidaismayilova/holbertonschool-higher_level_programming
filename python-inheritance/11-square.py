1cf684d464734b0f121abeee1da76ce1f27ebd03
saidaismayilova <iseide40@gmail.com>
2025-11-29 10:27:39 +0000

    salam

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square class inheriting from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize Square with size
+
+        Args:
+            size (int): Size of square; must be positive integer
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
