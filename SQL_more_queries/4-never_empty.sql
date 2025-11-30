c0963bd39b36fc7e09e49ac5261dc156fded8c27
saidaismayilova <iseide40@gmail.com>
2025-11-30 19:03:01 +0000

    s

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,12 +1,5 @@
--- Create the table 'id_not_null' if it does not exist
+-- salam
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
-
--- Regular inserts
-INSERT INTO id_not_null (id, name) VALUES (1, 'Holberton School');
-INSERT INTO id_not_null (id, name) VALUES (1, 'Python is cool');
-INSERT INTO id_not_null (id, name) VALUES (2, 'Holberton');
-INSERT INTO id_not_null (id, name) VALUES (3, 'School');
-INSERT INTO id_not_null (id, name) VALUES (4, 'C is fun');
