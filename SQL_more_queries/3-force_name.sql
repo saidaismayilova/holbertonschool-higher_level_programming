8b32d6d8413940150d683bb83f90e317266175b2
saidaismayilova <iseide40@gmail.com>
2025-11-30 19:27:32 +0000

    salam

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,12 +1,5 @@
--- Create the table 'force_name' if it does not exist
+-- salam
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
-
--- Regular inserts
-INSERT INTO force_name (id, name) VALUES (1, 'Holberton School');
-INSERT INTO force_name (id, name) VALUES (1, 'Python is cool');
-INSERT INTO force_name (id, name) VALUES (2, 'Holberton');
-INSERT INTO force_name (id, name) VALUES (3, 'School');
-INSERT INTO force_name (id, name) VALUES (4, 'C is fun');
