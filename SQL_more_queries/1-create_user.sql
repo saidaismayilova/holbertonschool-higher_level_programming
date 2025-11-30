986b4c38d12cbc9710a274b61d856fa7b01c1cfb
saidaismayilova <iseide40@gmail.com>
2025-11-30 18:48:13 +0000

    s

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates user_0d_1 with all privileges, does not fail if user exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
