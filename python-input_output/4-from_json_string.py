ce458e31d91eabf3d3898afba3789f58a055ecb4
saidaismayilova <iseide40@gmail.com>
2025-11-30 10:23:44 +0000

    s

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that returns a Python object from a JSON string."""
 import json
 
+
 def from_json_string(my_str):
-    """Returns an object represented by a JSON string."""
+    """Return the Python object represented by a JSON string."""
     return json.loads(my_str)
