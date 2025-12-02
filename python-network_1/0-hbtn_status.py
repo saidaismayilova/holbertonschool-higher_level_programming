00d93b89e90c8763fd73d3063969d708d05bce66
saidaismayilova <iseide40@gmail.com>
2025-12-02 12:37:09 +0000

    salam

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" this is path"""
-
+"""Fetches https://intranet.hbtn.io/status using urllib"""
 
 import urllib.request
-""" this is library .we must use urllib"""
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status')
-""" this is url """
-html = response.read()
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
