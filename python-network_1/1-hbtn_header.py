f1857443b4a6fa401bee4a83ce3f1822e1236f7b
saidaismayilova <iseide40@gmail.com>
2025-12-02 12:51:25 +0000

    s

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""this is path"""
-
+"""Fetches the X-Request-Id header from a given URL"""
 
 import urllib.request
 import sys
-if __name__ = "__main__"
-    url =  sys.argv[1]
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
     with urllib.request.urlopen(url) as response:
-        headers = response()info 
+        headers = response.info()
         print(headers.get("X-Request-Id"))
