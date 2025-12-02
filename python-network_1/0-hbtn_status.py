ba5e9be40d45327cdf0f26fb8227480473e1e362
saidaismayilova <iseide40@gmail.com>
2025-12-02 12:29:31 +0000

    s

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" this is path"""
+
+
 import urllib.request
+""" this is library .we must use urllib"""
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status')
+""" this is url """
 html = response.read()
