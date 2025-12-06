7ce3c311cff3007267d4dec22450905195b28803
saidaismayilova <iseide40@gmail.com>
2025-12-06 17:16:35 +0000

    s

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using requests package"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
