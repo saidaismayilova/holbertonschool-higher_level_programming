f4bec439357dcc8b00ebd84b51d94763fdb6b228
saidaismayilova <iseide40@gmail.com>
2025-12-06 17:38:07 +0000

    salam

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Fetch posts from JSONPlaceholder using requests library.
+Print titles and save posts into a CSV file.
+"""
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post.get("title"))
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        structured_posts = [{"id": p["id"], "title": p["title"], "body": p["body"]} for p in posts]
+
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for post in structured_posts:
+                writer.writerow(post)
